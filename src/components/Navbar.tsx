831f368f8de0658c357cddd7799fbf4ff0a8f98a
EgiDanuajisantosoo <oneeeback@gmail.com>
2025-07-02 13:54:15 +0700

    modifikasi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white p-4">
-      <ul className="flex gap-4">
+    <nav className="bg-blue-600 text-white p-4 fixed top-0 left-0 w-full z-50">
+      <ul className="flex justify-center gap-4 w-full">
         <li><a href="/">Beranda</a></li>
         <li><a href="/tentang">Tentang</a></li>
         <li><a href="/projects">Proyek</a></li>
