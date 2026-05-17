dfc06ffcf4971fb7a0f92753e0c0270c4afedd38
EgiDanuajisantosoo <oneeeback@gmail.com>
2026-05-17 20:34:48 +0700

    implement AIChatBot component with persistent session and markdown parsing

--- src/components/AIChatBot.tsx
+++ src/components/AIChatBot.tsx
@@ -408,7 +408,7 @@ export function AIChatBot() {
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   fill="currentColor"
-                  className="w-4.5 h-4.5 transform rotate-45 -translate-x-0.5 translate-y-0.5"
+                  className="w-4.5 h-4.5 transform rotate-0 -translate-x-0.10 translate-y-0.10"
                 >
                   <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
                 </svg>
